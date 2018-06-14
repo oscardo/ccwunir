67a339938baead5a4b9347c712de7333c6ec120e
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2018-06-14 11:12:40 -0500

    Initial commit

---
+++ app/src/main/java/co/unir/oscardo/ccwunir/Constantes/Publicos.java
@@ -0,0 +1,5 @@
+package co.unir.oscardo.ccwunir.Constantes;
+
+public final class Publicos {
+    public static int SPLASH_TIME_OUT = 1000;
+}
---
+++ app/src/main/java/co/unir/oscardo/ccwunir/SplashActivity.java
@@ -0,0 +1,25 @@
+package co.unir.oscardo.ccwunir;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.unir.oscardo.ccwunir.Constantes.Publicos;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, Publicos.SPLASH_TIME_OUT);
+    }
+}
