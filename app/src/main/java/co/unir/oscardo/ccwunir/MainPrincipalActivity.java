83bf732fc5ee69743ba75dddeb9d1f038311a347
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2018-06-18 13:17:45 -0500

    cambios de imagenes, texto y sentido de cada uno de los del menú izquierda como tal, además esta configurado el menú central con todos los datos

--- app/src/main/java/co/unir/oscardo/ccwunir/MainPrincipalActivity.java
+++ app/src/main/java/co/unir/oscardo/ccwunir/MainPrincipalActivity.java
@@ -67,7 +67,15 @@ public class MainPrincipalActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_configuracion) {
+            return true;
+        } else if(id == R.id.action_acerca){
+            return true;
+        } else if(id == R.id.action_busqueda){
+            return true;
+        } else if(id == R.id.action_politica){
+            return true;
+        } else if(id == R.id.action_salir){
             return true;
         }
 
